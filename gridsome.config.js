dcca952a1e2c0a49ee5c51b18cc18d665b521195
zwr <251592818@qq.com>
2021-05-13 10:50:26 +0800

    first

--- gridsome.config.js
+++ gridsome.config.js
@@ -20,7 +20,6 @@ module.exports = {
     {
       use: '@gridsome/source-strapi',
       options: {
-        // apiURL: 'http://localhost:1337',
         apiURL: process.env.GRIDSOME_API_URL,
         queryLimit: 1000, // Defaults to 100
         contentTypes: ['post','tag','journal-list','coverlist'],//查询的类型,拿取集合
