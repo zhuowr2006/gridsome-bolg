6fbe8f36f90789e9c5b2fab9e1f02ccdfdbd5c6d
zwr <251592818@qq.com>
2021-05-13 15:38:04 +0800

    first

---
+++ api/proxy.js
@@ -0,0 +1,28 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { createProxyMiddleware } = require('http-proxy-middleware')
+
+module.exports = (req, res) => {
+  let target = ''
+  
+  // 处理代理目标地址
+  // if (req.url.includes('/api/front')) {
+  //   target = 'http://edufront.lagou.com/'
+  // } else if (req.url.startsWith('/api/boss')) {
+  //   target = 'http://eduboss.lagou.com/'
+  // }
+  if (req.url.includes('/uploads')) {
+    target = 'http://1.116.145.169:1337/'
+  }
+  
+  // 创建代理对象并转发请求
+  createProxyMiddleware({
+    target,
+    changeOrigin: true,
+    // pathRewrite: {
+    //   // 通过路径重写，去除请求路径中的 /api
+    //   //   例如 /api/boss/xxx 将被转发到 http://eduboss.lagou.com/boss/xxx
+    //   //   例如 /api/front/xxx 将被转发到 http://eduboss.lagou.com/front/xxx
+    //   '^/api/': ''
+    // }
+  })(req, res)
+}
